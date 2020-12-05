81f4a02dfcb7e9b595eee9e227d344a9c115796e
ayangupt <ayangupt@users.noreply.github.com>
2020-12-04 16:24:42 -0800

    Created class DifficultyClassifier
    This class will parse a .txt document with words and classify then on a scale of 1-9 exponentially.

---
+++ src/com/company/DifficultyClassifier.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class DifficultyClassifier {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    // Sort all words exponentially on a scale of 1 to 9
+    // Place words into map with key being word and the associated <int> as the difficulty
+    // Sort map by key alphabetically
+    // Be able to perform binary search on map 
+}
